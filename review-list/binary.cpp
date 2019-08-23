c6d133cdb95f531eac6bcdbdded18c14e5fc6ed1
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-08-23 15:03:15 -0300

    Refatorando a conversão ede float decimal para binário

--- review-list/binary.cpp
+++ review-list/binary.cpp
@@ -6,50 +6,41 @@
 
 using namespace std;
 
-int main ()
-{
+int main (){
     float numero;
-	vector<int> binario;
+    vector<int> binario;
     vector<int>frac;
     cout << "Digite um numero inteiro" << endl; //printf("Digite um numero inteiro: "); 
     cin >> numero;								//scanf("%d", &numero);
-	
-	if (numero == 0)
-		binario.push_back(0);
-		
-	else
-	{
-        float decimal_part = floor(numero);
+    float decimal_part = floor(numero);
+    if (decimal_part == 0)
+        binario.push_back(0);
+    else{
         int n = decimal_part;
-        cout <<"decimal == "<< decimal_part << endl;
-		while (n != 1)
-		{
-			binario.push_back(n%2);
-			n = n / 2;
-		}
-		binario.push_back(1);
-		reverse(binario.begin(), binario.end());
-        
-        float fracional = modf(numero, &decimal_part);
-        cout << "fracional é =="<< fracional << endl;
-        float f = fracional;
-        while(f != 0.0){
-            fracional *= 2;
-            float p = floor(fracional);
-            int k = p;
-            frac.push_back(k%2);
-            f = modf(fracional, &p);
+        while (n != 1){
+            binario.push_back(n%2);
+            n = n / 2;
         }
-	}
-	
-	for (int i = 0; i < binario.size(); i++)
-	{
-		cout << binario[i];
-	}
-	cout << ".";
-    for (int i =0; i< frac.size(); i++)
-        cout << frac[i];
-    cout << endl;
-	
-	return 0;
+        binario.push_back(1);
+        reverse(binario.begin(), binario.end());
+    }
+    float fracional = modf(numero, &decimal_part);
+    int contador = 0;
+    while(fracional != 0 && contador < 5){
+        fracional *= 2;
+        float p = floor(fracional);
+        frac.push_back((int) p);
+        fracional = modf(fracional, &p);
+        contador++;
+    }
+    for (int i = 0; i < binario.size(); i++){
+        cout << binario[i];
+    }
+    if (frac.size() > 0){
+        cout << ".";
+        for (int i =0; i< frac.size(); i++)
+            cout << frac[i];
+        cout << endl;
+    }
+    return 0;
 } 
