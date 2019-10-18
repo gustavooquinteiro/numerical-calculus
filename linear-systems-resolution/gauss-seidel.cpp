f3e8f408e4a211b59b32c03a01e3f45d7126c14d
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-10-17 22:17:28 -0300

    Adicionando verificação de convergência usando o critério das linhas, o critério das colunas ou o critério de Sassenfeld

--- linear-systems-resolution/gauss-seidel.cpp
+++ linear-systems-resolution/gauss-seidel.cpp
@@ -1,13 +1,63 @@
  #include <bits/stdc++.h>
-
 using namespace std;
+#define MAX 1001
+#define LINE_CRITERIA_SATISFIED 10
+#define COLS_CRITERIA_SATISFIED 15
+#define NONE_CRITERIA_SATISFIED 5
+#define SASSENFELD_CRITERIA_SATISFIED 20
+float coeficientes[MAX][MAX];
+
+
+int sassenfeld_criteria(int m){
+    float beta[m] = {};
+    for (int i = 0; i < m; i++){
+        float somatorio = 0;
+        for (int j = 0; j < i; j++){
+            somatorio += coeficientes[i][j] * beta[j];
+        }
+        for (int j = i+1; j < m; j++){
+            somatorio += coeficientes[i][j];
+        }
+        beta[i] = somatorio / coeficientes[i][i];
+    }
+    sort(beta, beta+m);
+    if (beta[m-1] < 1)
+        return SASSENFELD_CRITERIA_SATISFIED;
+    return NONE_CRITERIA_SATISFIED;
+}
+
+
+int criteria(int m){
+    float lines[m] = {};
+    float cols[m] = {};
+    int indice = 0;
+    for(int i = 0; i < m; i++){
+        float somatorio_line = 0;
+        float somatorio_cols = 0;
+        for(int j = 0; j < m; j++){
+            if (i != j){
+                somatorio_line += coeficientes[i][j];
+                somatorio_cols += coeficientes[j][i];
+            }
+        }
+        lines[i] = somatorio_line / coeficientes[i][i];
+        cols[i] = somatorio_cols / coeficientes[i][i];
+    }
+    sort(lines, lines+m);
+    sort(cols, cols+m);
+    if (lines[m-1] < 1)
+        return LINE_CRITERIA_SATISFIED;
+    if (cols[m-1] < 1)
+        return COLS_CRITERIA_SATISFIED;
+    return sassenfeld_criteria(m);
+}
+
 
 int main(){
     int m;
     float epsilon;
     cin >> m;
     cin >> epsilon;
-    float coeficientes[m][m];
     int termos_independentes[m];
     for (int x = 0; x < m; x++){
         for(int y = 0; y < m; y++){
@@ -16,7 +66,20 @@ int main(){
     }
     for (int x = 0; x < m; x++)
         cin >> termos_independentes[x];
-    
+    switch(criteria(m)){
+        case LINE_CRITERIA_SATISFIED:
+            cout << "Criterio das linhas satisfeito\n";
+            break;
+        case COLS_CRITERIA_SATISFIED:
+            cout << "Criterio das colunas satisfeito\n";
+            break;
+        case SASSENFELD_CRITERIA_SATISFIED:
+            cout << "Criterio de Sassenfeld satisfeito\n";
+            break;
+        default:
+            cout << "Nenhum criterio satisfeito\n";
+            break;
+    }
     float x_barra[m] = {};
     float x_atual[m] = {};
     float erro_relativo = 20000000000000;
--- linear-systems-resolution/jacobi.cpp
+++ linear-systems-resolution/jacobi.cpp
@@ -1,15 +1,42 @@
  #include <bits/stdc++.h>
-
 using namespace std;
+#define MAX 1001
+#define LINE_CRITERIA_SATISFIED 10
+#define COLS_CRITERIA_SATISFIED 15
+#define NONE_CRITERIA_SATISFIED 5
+float coeficientes[MAX][MAX];
 
+int criteria(int m){
+    float lines[m] = {};
+    float cols[m] = {};
+    int indice = 0;
+    for(int i = 0; i < m; i++){
+        float somatorio_line = 0;
+        float somatorio_cols = 0;
+        for(int j = 0; j < m; j++){
+            if (i != j){
+                somatorio_line += coeficientes[i][j];
+                somatorio_cols += coeficientes[j][i];
+            }
+        }
+        lines[i] = somatorio_line / coeficientes[i][i];
+        cols[i] = somatorio_cols / coeficientes[i][i];
+    }
+    sort(lines, lines+m);
+    sort(cols, cols+m);
+    if (lines[m-1] < 1)
+        return LINE_CRITERIA_SATISFIED;
+    if (cols[m-1] < 1)
+        return COLS_CRITERIA_SATISFIED;
+    return NONE_CRITERIA_SATISFIED;
+}
 
 
 int main(){
-    int m;
+    int m ;
     int n;
     cin >> m;
     cin >> n;
-    float coeficientes[m][m];
     int termos_independentes[m];
     for (int x = 0; x < m; x++){
         for(int y = 0; y < m; y++){
@@ -18,7 +45,17 @@ int main(){
     }
     for (int x = 0; x < m; x++)
         cin >> termos_independentes[x];
-    
+    switch(criteria(m)){
+        case LINE_CRITERIA_SATISFIED:
+            cout << "Criterio das linhas satisfeito\n";
+            break;
+        case COLS_CRITERIA_SATISFIED:
+            cout << "Criterio das colunas satisfeito\n";
+            break;
+        default:
+            cout << "Nenhum criterio satisfeito\n";
+            break;
+    }
     float x_barra[m] = {};
     float resposta[m] = {};
      for (int i = 0; i < n + 1; i++){
@@ -35,7 +72,7 @@ int main(){
         for (int a = 0; a < m; a++)
             x_barra[a] = resposta[a];
      }
-cout << "resposta do sistema após " << n << " iterações" << endl;
+    cout << "resposta do sistema após " << n << " iterações" << endl;
      for (int i = 0; i < m; i++)
          cout << x_barra[i] << " ";
      cout << endl;
