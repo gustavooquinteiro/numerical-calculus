1201dc9a9a59a01ae961210843a504e50597c6ab
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-10-30 09:05:28 -0300

    Consertando o algoritmo de preenchimento da tabela de diferenças dividas. Mudando para double todos os floats

--- interpolation/newton-formula.cpp
+++ interpolation/newton-formula.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 1000
-float tabela[MAX][2];
-float diferenca[MAX][MAX];
+double tabela[MAX][2];
+double diferenca[MAX][MAX];
 
 void preenche_tabela(int n){
     for (int i = 0; i < n; i ++){
@@ -11,50 +11,35 @@ void preenche_tabela(int n){
         }
     }
     
-     for (int i = 0; i < n; i ++){
-        for (int j = 0; j < 2; j++){
-            cout << tabela[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "-----------\n";
-    
-    
     int ordem = 1;
     for (int i = 0; i < n - 1; i ++){
-        diferenca[i][ordem] = (tabela[i + 1][1] - tabela[i][1]) / (tabela[i+ordem][0] - tabela[i][0]); 
+        double denominador = tabela[i+ordem][0] - tabela[i][0];
+        double numerador = tabela[i+1][1] - tabela[i][1];
+        diferenca[i][ordem] = numerador / denominador;
     }
     ordem++;
-    for (int j = ordem; j < n-1; j++){
-        for (int i = 0; i < n; i++){
-            cout << i << " e "<< j << endl;
-            cout <<  (diferenca[i+1][1] - diferenca[i][1]) << endl;
-            cout << (tabela[i+j][0] - tabela[i][0]) << endl;
-            diferenca[i][j] = (diferenca[i+1][j-1] - diferenca[i][j-1]) / (tabela[i+j][0] - tabela[i][0]);
+    int diff = 2;
+    for (int j = ordem; j < n; j++){
+        for (int i = 0; i < n - diff; i++){
+            double denominador = tabela[i+j][0] - tabela[i][0];
+            double numerador = diferenca[i+1][j-1] - diferenca[i][j-1];
+            diferenca[i][j] = numerador/denominador;
         }
-    }
-    
-    
-    for (int i = 0; i < n-1; i ++){
-        for (int j = 0; j < n-1; j++){
-            cout << diferenca[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
+        diff++;
+    }    
 }
 
 
-void newton_interpolation_formula(float x, int n){
-    int termo = tabela[0][1];
-    float somatoria = 0;
+void newton_interpolation_formula(double x, int n){
+    double termo = tabela[0][1];
+    double somatoria = 0;
     for (int i = 1; i < n; i++){
-        float produtoria = 1;
-        for (int j = 0; j < i-1; j++){
+        double produtoria = 1;
+        for (int j = 0; j < i; j++){
             produtoria *= (x - tabela[j][0]);
         }
-        cout << produtoria << endl;
         somatoria += diferenca[0][i] * produtoria;
+        
     }
     somatoria += termo;
     cout << somatoria << endl;
@@ -64,7 +49,7 @@ int main(){
     int n;
     cin >> n;
     preenche_tabela(n);
-    float x;
+    double x;
     cin >> x;
     newton_interpolation_formula(x, n);
 }
