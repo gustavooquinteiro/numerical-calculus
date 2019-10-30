82596ac376ab71538a7a337d0992d95bca11c7b3
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2019-10-30 09:08:48 -0300

    Adicionando macro n

--- interpolation/lagrange.cpp
+++ interpolation/lagrange.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-float matriz[4][2];
+#define n 3
+float matriz[n][2];
 
 void preencher(){
-	for(int i = 0; i < 4; i++){
+	for(int i = 0; i < n; i++){
 		for (int j =0; j < 2; j++){
 			cin >> matriz[i][j];
 		}
@@ -12,17 +13,15 @@ void preencher(){
 }
 
 float calculo(float x){
-	float prod[4] = {1,1,1,1};
 	float sum =0;
-	for (int i = 0; i < 4; i++){
-		for (int j = 0; j < 4; j++){
+	for (int i = 0; i < n; i++){
+        float prod = 1;
+		for (int j = 0; j < n; j++){
 			if (i != j){
-				prod[i] *= ((x - matriz[j][0])/(matriz[i][0] - matriz[j][0]));
+				prod *= ((x - matriz[j][0])/(matriz[i][0] - matriz[j][0]));
 			}
 		}
-	}
-	for (int i = 0; i < 4; i++){
-		sum += matriz[i][1] * prod[i];
+		sum += matriz[i][1] * prod;
 	}
 	return sum;
 }
