a6e50edfc42bf1dbf2e03b2d03faa85cb2913a98
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-16 10:49:10 +0000

    Adicionando a Primeira Regra de Simpson Composta

---
+++ numerical-integration/first-simpson-rule.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double f(double x){
+    return pow(x,3) + pow(x, 2) + x + 1;
+}
+
+double first_simpson_rule(double a, double b, int n){
+    double h = (b-a)/n;
+    double x[n] = {};
+    double result = 0;
+    int cont = 0;
+    for (double i = a; i < b; i += h)        
+        x[cont++] = i;
+    double somatorio = 0;
+    for (int i =1; i< n/2; i++){
+        somatorio += f(x[2*i-1]);
+    }
+    result += 4*somatorio;
+    somatorio = 0;
+    for (int i =1; i< n/2; i++){
+        somatorio += f(x[2*i]);    
+    }
+    result += 2*somatorio;
+    return (h/3)*(result + f(x[0]) + f(x[n]));
+}
+
+int main(){
+    double a, b;
+    cin >> a >> b;
+    int n;
+    cin >> n;
+    cout <<first_simpson_rule(a, b, n) << endl;
+}
